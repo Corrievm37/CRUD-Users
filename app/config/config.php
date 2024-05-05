49635e49d1e4a11336f1c94c0c3a4408159d74c2
Corrievm37 <86923318+Corrievm37@users.noreply.github.com>
2024-05-05 14:59:42 +0200

    Upload New
    New Upoad

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'corriecrud');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', '127.0. 0.1');
+  // Site Name
+  define('SITENAME', 'Corrie Crud');
+  // App Version
+  define('APPVERSION', '1.0.0');
